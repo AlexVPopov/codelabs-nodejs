362a73759cc63b93ec75f79b7f373ced4ff79db4
Alexander Popov <alex.val.popov@gmail.com>
2019-02-04 14:36:37 +0200

    Personalize

--- level2/functions/index.js
+++ level2/functions/index.js
@@ -13,22 +13,42 @@
 
 'use strict';
 
-// Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const {
+  dialogflow,
+  Permission,
+  Suggestions,
+} = require('actions-on-google');
 
-// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
-// Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
-// Handle the Dialogflow intent named 'favorite color'.
-// The intent collects a parameter named 'color'.
 app.intent('favorite color', (conv, {color}) => {
     const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
+
+    if (conv.data.userName) {
+      conv.close(`${conv.data.userName}, your lucky number is ${luckyNumber}`);
+    } else {
+      conv.close(`Your lucky number is ${luckyNumber}.`);
+    }
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+app.intent('Default Welcome Intent', (conv) => {
+  conv.ask(new Permission({
+    context: 'Hi there, to get to know you better',
+    permissions: 'NAME'
+  }));
+});
+
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+  if (permissionGranted) {
+    conv.data.userName = conv.user.name.dispaly;
+    conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+  } else {
+    conv.ask(`Ok, no worries. What's your favorite color?`);
+  }
+  conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+});
