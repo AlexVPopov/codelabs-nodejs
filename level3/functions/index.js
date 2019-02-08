5399714c3b761fe255699fd110a8925e895efa12
Alexander Popov <alex.val.popov@gmail.com>
2019-02-08 17:09:59 +0200

    Replace conv.data with conv.user.storage

--- level3/functions/index.js
+++ level3/functions/index.js
@@ -61,11 +61,16 @@ const colorMap = {
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
-  // Asks the user's permission to know their name, for personalization.
-  conv.ask(new Permission({
-    context: 'Hi there, to get to know you better',
-    permissions: 'NAME',
-  }));
+ const name = conv.user.storage.userName;
+ if (!name) {
+   // Asks the user's permission to know their name, for personalization.
+   conv.ask(new Permission({
+     context: 'Hi there, to get to know you better',
+     permissions: 'NAME',
+   }));
+ } else {
+   conv.ask(`Hi again, ${name}. What's your favorite color?`);
+ }
 });
 
 // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
@@ -78,8 +83,8 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
   } else {
     // If the user accepted our request, store their name in
     // the 'conv.data' object for the duration of the conversation.
-    conv.data.userName = conv.user.name.display;
-    conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+    conv.user.storage.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.user.storage.userName}. What's your favorite color?`);
     conv.ask(new Suggestions('Blue', 'Red', 'Green'));
   }
 });
@@ -89,10 +94,10 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
 app.intent('favorite color', (conv, {color}) => {
   const luckyNumber = color.length;
   const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
-  if (conv.data.userName) {
+  if (conv.user.storage.userName) {
     // If we collected user name previously, address them by name and use SSML
     // to embed an audio snippet in the response.
-    conv.ask(`<speak>${conv.data.userName}, your lucky number is ` +
+    conv.ask(`<speak>${conv.user.storage.userName}, your lucky number is ` +
       `${luckyNumber}.<audio src="${audioSound}"></audio> ` +
       `Would you like to hear some fake colors?</speak>`);
     conv.ask(new Suggestions('Yes', 'No'));
